158a51a5ab672cd092449e38e3623a1cf651cc0e
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2022-05-16 09:48:05 -0500

    created heroes and hero-detail components

---
+++ src/app/hero.ts
@@ -0,0 +1,12 @@
+/*
+*
+* Interface provides the structure of a hero object but not the implementation
+* Only used to define the shape of the object for developers
+* Not used by Angular at runtime, only for type checking
+* Removed when transpiled to JavaScript
+*/
+
+export interface Hero {
+  id: number;
+  name: string;
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes', // component css selector
+  templateUrl: './heroes.component.html', // location of the component's template file
+  styleUrls: ['./heroes.component.css'] // location of css file
 })
 export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
+  heroes = HEROES;
 
   constructor() { }
 
   ngOnInit(): void {
+    // life cycle hook that runs when component is initialized
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
